8bbd76dd00cefc6232c442dbc2e4db076e477ced
tinishad101-hub <tinishad101@gmail.com>
2026-05-13 16:06:38 +0530

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,9 @@
+            /*!
+ * Swiper 11.0.0 - Minimal JS mock for offline testing
+ */
+class Swiper {
+    constructor(selector, options) {
+      console.log("Swiper initialized:", selector, options);
+    }
+  }
+  
